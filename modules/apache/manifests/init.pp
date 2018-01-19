c265e0a32755bc4ce2fe74d3dac7ee025911a902
nkafsal22 <nkafsal22@gmail.com>
2018-01-19 14:43:24 +0530

    apache change

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,9 +1,11 @@
-class apache  {
+class apache (
+  $apachename   = $::apache::params::apachename,
+) inherits ::apache::params   {
 exec { 'apt-update':
    command => '/usr/bin/apt-get update'
      }
 package { 'apache':
-     name => 'apache2',
+     name => $apachename,
      require => Exec['apt-update'],
      ensure => installed, }
 file { '/etc/apache2/apache2.conf':
